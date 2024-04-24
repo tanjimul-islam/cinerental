0386797f6c1293530394cba07c534f2eca3c08be
Tanjimul Islam <tanjimul.islam@hotmail.com>
2024-04-24 11:49:29 +0600

    remove cart

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import MovieRating from "../components/MovieRating";
+import { MovieContext } from "../context";
 import { getImgUrl } from "../utils/cine-utility";
 import MovieDetailsModal from "./MovieDetailsModal";
-
 const MovieCard = ({ movie }) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const { cartData, setCartData } = useContext(MovieContext);
   const handleModalClose = () => {
     setSelectedMovie(null);
     setShowModal(false);
@@ -14,10 +15,26 @@ const MovieCard = ({ movie }) => {
     setSelectedMovie(movie);
     setShowModal(true);
   };
+  const handleAddToCart = (e, movie) => {
+    e.stopPropagation();
+
+    const found = cartData.find((item) => {
+      return item.id === movie.id;
+    });
+    if (!found) {
+      setCartData([...cartData, movie]);
+    } else {
+      console.error(`This movie ${movie.title} has already been added`);
+    }
+  };
   return (
     <>
       {showModal && (
-        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+        <MovieDetailsModal
+          movie={selectedMovie}
+          onClose={handleModalClose}
+          onCartAdd={handleAddToCart}
+        />
       )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <a onClick={() => handleMovieSelection(movie)} href="#">
@@ -26,21 +43,22 @@ const MovieCard = ({ movie }) => {
             src={getImgUrl(movie.cover)}
             alt=""
           />
-          <figcaption className="pt-4">
-            <h3 className="text-xl mb-1">{movie.title}</h3>
-            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-            <div className="flex items-center space-x-1 mb-5">
-              <MovieRating value={movie.rating} />
-            </div>
-            <a
-              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-              href="#"
-            >
-              <img src="./assets/tag.svg" alt="" />
-              <span>${movie.price} | Add to Cart</span>
-            </a>
-          </figcaption>
         </a>
+        <figcaption className="pt-4">
+          <h3 className="text-xl mb-1">{movie.title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+          <div className="flex items-center space-x-1 mb-5">
+            <MovieRating value={movie.rating} />
+          </div>
+          <a
+            onClick={(e) => handleAddToCart(e, movie)}
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+          >
+            <img src="./assets/tag.svg" alt="" />
+            <span>${movie.price} | Add to Cart</span>
+          </a>
+        </figcaption>
       </figure>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { useContext, useState } from "react";
 import moon from "../assets/icons/moon.svg";
 import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "../cine/CartDetails";
+import { MovieContext } from "../context";
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+  const handleCartShow = () => {
+    setShowCart(true);
+  };
+  const { cartData } = useContext(MovieContext);
+  console.log(cartData);
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
@@ -31,8 +41,14 @@ const Header = () => {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleCartShow}
             >
               <img src={shoppingCart} width="24" height="24" alt="" />
+              {cartData.length > 0 && (
+                <span className="rounded-full absolute top-[-12px] left-[28px] bg-[#12CF6F] text-white text-center p-[2px] w-[30px] h-[30px] ">
+                  {cartData.length}
+                </span>
+              )}
             </a>
           </li>
         </ul>
